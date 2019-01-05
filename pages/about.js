29749e803e53052a9c6785d8f3915dd8bd6b1d37
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-01-05 20:01:56 +0100

    project complete

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,12 @@
-export default () => <div>About BitzPrice</div>
+import Layout from '../components/Layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices</p>
+    </div>
+  </Layout>
+)
+
+export default About
